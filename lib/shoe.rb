9cc7e7f9e71f4ac84dbf3af9fd59674f58b15fd1
Riley Moore <rilemoore@gmail.com>
2019-10-16 20:43:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,13 @@
 # Make your shoe class here!
 Class shoe
   attr_accessor :size, :color, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
 end
